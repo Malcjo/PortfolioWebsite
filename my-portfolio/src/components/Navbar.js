08592070d501df11e7e235723dcab31c6ea509cf
Malcjo <joshuaeric.malcolm@gmail.com>
2024-04-20 17:29:51 +1200

    setup part 12

---
+++ my-portfolio/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Navbar(){
+    return (
+        <section id = "about">
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+
+            </div>
+        </section>
+    );
+}
