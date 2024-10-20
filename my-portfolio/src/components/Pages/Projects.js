9e35c6837d0bd623faac1f9896a4480d902a840d
Malcjo <joshuaeric.malcolm@gmail.com>
2024-10-20 22:11:45 +1300

    adjusting projects sub nav bar

--- my-portfolio/src/components/Pages/Projects.js
+++ my-portfolio/src/components/Pages/Projects.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Projects() {
   return (
     <section className="text-gray-400 body-font">
-      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+      <div className="container mx-auto px-5 sm:px-4 md:px-8 lg:px-16 xl:px-24 py-10  text-center">
         <div className="flex flex-col w-full mb-20">
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
             Projects
@@ -12,7 +12,7 @@ export default function Projects() {
         </div>
 
         {/* Sub Navbar */}
-        <div className="flex justify-center space-x-4 mb-8">
+        <div className="flex flex-wrap justify-center gap-4 mb-8">
           <Link to="/projects/webdev" className="px-4 py-2 bg-gray-800 text-blue-400 hover:text-white">Web Development</Link>
           <Link to="/projects/softwaredev" className="px-4 py-2 bg-gray-800 text-blue-400 hover:text-white">Other Software Development</Link>
           <Link to="/projects/gamedev" className="px-4 py-2 bg-gray-800 text-blue-400 hover:text-white">Game Development</Link>
